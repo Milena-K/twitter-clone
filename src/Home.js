39d80e8a1f7979be2be5c2bfd30a9e086cc87399
Milena-K <mkukolj@protonmail.com>
2022-10-13 10:25:58 +0200

    Fetched tweets from db.

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,26 @@
+import { useReducer, useState, useEffect } from 'react';
+import { applyActionCode, getAuth } from 'firebase/auth';
+import { query, collection, getFirestore, orderBy, limit, onSnapshot } from 'firebase/firestore';
 import NewTweet from "./NewTweet";
+import Tweet from "./Tweet";
 import './styles/Home.css'
+
 const Home = () => {
+  const [loaded, setLoaded] = useState(false);
+  const [tweets, setTweets] = useState();
+
+  // Functions
+  useEffect(() => {
+    var data = []
+    const recentTweetsQuery = query(collection(getFirestore(), 'tweets'), limit(30));
+    onSnapshot(recentTweetsQuery, function(snapshot) {
+      snapshot.docChanges().forEach(function(change) {
+        data = [...data, change.doc.data()]
+        setTweets(data);
+        setLoaded(true);
+      })
+    })
+  }, [loaded])
 
   return (
     <div id='HomeContainer'>
@@ -12,7 +32,11 @@ const Home = () => {
       <div
         className="thin-borders"
         id="TweetsContainer">
-        the tweets
+        {
+          loaded && tweets.map(tweet => {
+            return <Tweet title={tweet.text} />
+          })
+        }
       </div>
     </div>
   )
---
+++ src/Tweet.js
@@ -0,0 +1,12 @@
+
+const Tweet = (props) => {
+
+  return (
+    <div>
+      <p>hi</p>
+      <p>{props.title}</p>
+    </div>
+  )
+}
+
+export default Tweet;
